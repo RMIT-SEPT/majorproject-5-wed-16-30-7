24a8bc6b1bbe78aad81c3a76b87b431320f27e51
s3719421 <s3719421@student.rmit.student.edu.au>
2020-08-19 18:24:21 +1000

    console log user input

--- FrontEnd/agme-app/src/Components/LoginForm.js
+++ FrontEnd/agme-app/src/Components/LoginForm.js
@@ -1,65 +1,76 @@
 import React from 'react';
 import './LoginForm.scss';
 class LoginForm extends React.Component {
-    
-    constructor(props){
+
+    constructor(props) {
         super(props);
         this.state = {
-            username: null,
-            password: null,
+            username: '',
+            password: '',
 
             errors: {
                 username: '',
                 password: '',
             }
         };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.getCustomerLogin = this.getCustomerLogin.bind(this);
+    }
+
+    getCustomerLogin = (e) => {
+        e.preventDefault();
+        let customer = { username: this.state.username, password: this.state.password };
+        console.log('customerLogin =>' + JSON.stringify(customer));
     }
 
     handleChange = (event) => {
         event.preventDefault();
-        const {name, value} = event.target;
+        const { name, value } = event.target;
         let errors = this.state.errors;
 
-        switch (name){
+        switch (name) {
             case 'username':
-                errors.username = 
-                value.length < 5
-                ? 'Please enter a valid username'
-                :'';
-            break;
+                errors.username =
+                    value.length < 5
+                        ? 'Please enter a valid username'
+                        : '';
+                break;
 
             case 'password':
-                errors.password = 
-                value.length < 3
-                ? 'Please enter a valid password'
-                :'';
-            break;
+                errors.password =
+                    value.length < 3
+                        ? 'Please enter a valid password'
+                        : '';
+                break;
 
             default:
-            break;
+                break;
         }
 
-        this.setState({errors, [name]: value}, ()=> {
-            console.log(errors)
+        this.setState({ errors, [name]: value }, () => {
+            // console.log(errors)
         })
     }
 
     render() {
-        const {errors} = this.state;
+        const { errors } = this.state;
         return (
             <div>
                 <form className="login-form">
                     <h1>Login</h1>
                     <div className="form-input-material">
                         <label className="label" for="username">Username</label>
-                        <input type='text' name='username' id='username' placeholder="Enter Username " className="form-control-material" onChange = {this.handleChange} required />
+                        <input type='text' name='username' id='username' placeholder="Enter Username " className="form-control-material"
+                            value={this.state.username} onChange={this.handleChange} required />
                     </div>
 
                     <div className="form-input-material">
                         <label className="label" for="password">Password</label>
-                        <input type='password' name='password' placeholder=" " className="form-control-material" onChange = {this.handleChange} required />
+                        <input type='password' name='password' placeholder=" " className="form-control-material"
+                            value={this.state.password} onChange={this.handleChange} required />
                     </div>
-                    <button type="submit" class="btn btn-primary btn-ghost" >Login</button>
+                    <button type="submit" className="btn btn-primary btn-ghost" onClick={this.getCustomerLogin}>Login</button>
                 </form>
             </div>
         );
