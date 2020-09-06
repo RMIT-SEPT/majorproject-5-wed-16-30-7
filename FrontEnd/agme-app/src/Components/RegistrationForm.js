3d52f3ed5903a5e9246c548025707ce148c7846b
s3719421 <s3719421@student.rmit.student.edu.au>
2020-09-06 22:12:49 +1000

    register form added

--- FrontEnd/agme-app/src/Components/RegistrationForm.js
+++ FrontEnd/agme-app/src/Components/RegistrationForm.js
@@ -1,36 +1,55 @@
 import React from 'react';
 import './LoginForm.scss';
-class LoginForm extends React.Component {
+
+class RegistrationForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            address: '',
+            phone: '',
+            username: '',
+            password: '',
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.getCustomerRegistration = this.getCustomerRegistration.bind(this);
+    }
+
+    getCustomerRegistration = (e) => {
+        e.preventDefault();
+        let customer = {
+            name: this.state.name,
+            address: this.state.address,
+            phone: this.state.phone,
+            username: this.state.username,
+            password: this.state.password
+        };
+        console.log('customerLogin =>' + JSON.stringify(customer));
+    }
+
+    handleChange = (event) => {
+        event.preventDefault();
+        const { name, value } = event.target;
+
+        this.setState({ [name]: value }, () => {
+        })
+    }
 
     render() {
-        <div>
-            <form className="login-form">
-                <h1>Registration</h1>
-                <div className="form-input-material">
-                    <label className="label" for="name">Name</label>
-                    <input type='text' name='name' id='name' placeholder="Enter Name " className="form-control-material"
-                        value={this.state.username} onChange={this.handleChange} required />
-                </div>
-
-                <div className="form-input-material">
-                    <label className="label" for="email">Email</label>
-                    <input type='email' name='email' placeholder=" " className="form-control-material"
-                        value={this.state.password} onChange={this.handleChange} required />
-                </div>
-
-                <div className="form-input-material">
-                    <label className="label" for="password">Password</label>
-                    <input type='password' name='password' placeholder=" " className="form-control-material"
-                        value={this.state.password} onChange={this.handleChange} required />
-                </div>
-
-                <div className="form-input-material">
-                    <label className="label" for="password-confirm">Confirm Password</label>
-                    <input type='password' name='password-confirm' placeholder=" " className="form-control-material"
-                        value={this.state.password} onChange={this.handleChange} required />
-                </div>
-                <button type="submit" className="btn btn-primary btn-ghost" onClick={this.getCustomerLogin}>Login</button>
+        return (
+            <form id="register" className="input-group">
+                <input type="text" className="input-field" name='name' placeholder="Full Name" value={this.state.name} onChange={this.handleChange} required />
+                <input type="text" className="input-field" name='address' placeholder="Address" value={this.state.address} onChange={this.handleChange} required />
+                <input type="text" className="input-field" name='phone' placeholder="Phone Number" value={this.state.phone} onChange={this.handleChange} required />
+                <input type="text" className="input-field" name='username' placeholder="User Name" value={this.state.username} onChange={this.handleChange} required />
+                <input type="password" className="input-field" placeholder="Password" required />
+                <input type="password" className="input-field" name='password' placeholder="Confirm Password" value={this.state.password} onChange={this.handleChange} required />
+                <button type="submit" className="submit-btn" onClick={this.getCustomerRegistration}>Register</button>
             </form>
-        </div>
+        );
     }
 }
+
+export default RegistrationForm;
