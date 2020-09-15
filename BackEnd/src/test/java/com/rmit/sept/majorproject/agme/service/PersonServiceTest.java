05fe9c6ed2b6d92d5eb3289fe4bbeacbf30dd8e4
sean8900 <s3806690@student.rmit.edu.au>
2020-09-15 10:32:51 +1000

    implementing springboot unit tests

---
+++ BackEnd/src/test/java/com/rmit/sept/majorproject/agme/service/PersonServiceTest.java
@@ -0,0 +1,44 @@
+package com.rmit.sept.majorproject.agme.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonServiceTest {
+
+	@Test
+	void login() {
+	}
+
+	@Test
+	void getPersonAccountTypeById() {
+	}
+
+	@Test
+	void addPerson() {
+	}
+
+	@Test
+	void getPersonById() {
+	}
+
+	@Test
+	void getPersonByUsername() {
+	}
+
+	@Test
+	void personIdExist() {
+	}
+
+	@Test
+	void updatePerson() {
+	}
+
+	@Test
+	void updatePersonPassword() {
+	}
+
+	@Test
+	void deletePersonById() {
+	}
+}
