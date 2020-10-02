d8148adcec66a232dcd4ecf168ed16b1452d1c11
s3719421 <s3719421@student.rmit.student.edu.au>
2020-10-02 18:10:20 +1000

    Added booking page

---
+++ FrontEnd/agme-app/src/Components/BookingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class BookingPage extends React.Component {
+
+    render() {
+        return (
+            <di><h2>Booking Page</h2></di>
+        )
+    }
+}
+
+export default BookingPage;
