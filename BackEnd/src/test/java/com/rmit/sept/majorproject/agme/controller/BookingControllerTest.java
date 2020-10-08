50c5a3679b4c015b10263faefe08053d2894d71f
sean8900 <s3806690@student.rmit.edu.au>
2020-10-08 15:25:54 +1100

    modified test during lab with tutor

--- BackEnd/src/test/java/com/rmit/sept/majorproject/agme/controller/BookingControllerTest.java
+++ BackEnd/src/test/java/com/rmit/sept/majorproject/agme/controller/BookingControllerTest.java
@@ -9,7 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -21,7 +21,7 @@ import java.util.stream.Stream;
 import static org.assertj.core.util.DateUtil.now;
 import static org.mockito.Mockito.when;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookingControllerTest {
 	private MockMvc mockMvc;
